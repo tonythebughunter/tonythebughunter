10512b8dc163b1196e5fc869f89688b7b5717071
tonythebughunter <144837471+tonythebughunter@users.noreply.github.com>
2023-09-17 12:37:52 +0300

    Create random_password.py
    generate random password using this python script. You can set length to your desired password length

---
+++ random_password.py
@@ -0,0 +1,9 @@
+import random
+upper="ABCDEFGHIJKLMNOQRSTUVWXYZ"
+lower='abcefghijklmnopqrstuvwxyz"
+numbers="0123456789"
+special="!@#$%^&*()-+{}?/"
+length=15
+add=upper+lower+numbers+special
+password="".join(random.sample(add,length))
+print(password)
