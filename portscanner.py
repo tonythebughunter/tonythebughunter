00d32750d63fd06fb05b932813b1eec8e954e63c
tonythebughunter <144837471+tonythebughunter@users.noreply.github.com>
2023-12-28 11:30:03 +0300

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -1,17 +1,32 @@
 import socket
+from IPy import IP
+import sys
+
 def portscan(ipaddress, port):
-  try:
-    sock=socket.socket()
-    sock.settimeout(0.5)
-    sock.connect((ipaddress, port))
-    print("port " + str(port) + " is open");
-  except:
-    n=0;
-ipaddress=input("[+]Enter the target ipaddress or url: ")
-a=int(input("[+]Enter the first port to scan: "))
-b=int(input("[+]Enter the final port to scan: "))
-print("Scanning " + ipaddress + " ...")
-for port in range(a, b+1):
-  portscan(ipaddress, port);
-    
+    try:
+        sock = socket.socket()
+        sock.settimeout(0.5)
+        sock.connect((ipaddress, port))
+        print("Port " + str(port) + " is open")
+    except socket.timeout:
+        pass
+    except KeyboardInterrupt:
+        print("\nScan interrupted by user.")
+        sys.exit()  # Exit the script
+    finally:
+        sock.close()
+
+try:
+    ipaddress = input("[+] Enter the target IP address or URL: ")
+    a = int(input("[+] Enter the first port to scan: "))
+    b = int(input("[+] Enter the final port to scan: "))
+    print("Scanning " + str(ipaddress))
+
+    for port in range(a, b):
+        portscan(ipaddress, port)
+
+except KeyboardInterrupt:
+    print("\nScan interrupted by user.")
+    sys.exit()  # Exit the script
+
      
